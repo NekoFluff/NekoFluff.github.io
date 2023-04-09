f757f094747a01c4d6bd9e70e51d92001a2bd8c7
Alex Nou <alexnou@gmail.com>
2023-04-09 15:11:34 -0700

    fix: fix typescript library compiler options

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,23 +2,26 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  root: true,
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier",
-  ],
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
-        "cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}",
-      ],
-      extends: ["plugin:cypress/recommended"],
+    env: {
+        "es2021": true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
+    root: true,
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-typescript",
+        "@vue/eslint-config-prettier",
+    ],
+    overrides: [
+        {
+            files: [
+                "**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
+                "cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}",
+            ],
+            extends: ["plugin:cypress/recommended"],
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+    },
 };
