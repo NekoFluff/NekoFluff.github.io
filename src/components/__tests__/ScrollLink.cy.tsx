869403a472da0b742c3c55bba43c6239285cfa3f
Alex Nou <alexnou@gmail.com>
2023-04-09 20:35:09 -0700

    test: add tests for ScrollLink component

--- src/components/__tests__/ScrollLink.cy.tsx
+++ src/components/__tests__/ScrollLink.cy.tsx
@@ -24,4 +24,26 @@ describe('ScrollLink', () => {
         cy.get('@scrollIntoView').should('have.been.called')
         cy.get('@callback').should('have.been.called')
     })
+
+    it('should not call the callback when the target element is not found', () => {
+        cy.document().then((doc) => {
+            const el = doc.getElementById('target')
+            if (el) {
+                el.remove()
+            }
+        })
+
+        cy.mount(ScrollLink, {
+            props: {
+                targetId: 'target',
+                callback: cy.spy().as('callback'),
+            },
+            slots: {
+                default: 'Click me',
+            },
+        })
+
+        cy.get('button').click()
+        cy.get('@callback').should('not.have.been.called')
+    })
 })
